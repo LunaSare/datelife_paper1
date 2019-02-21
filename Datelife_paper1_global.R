177cdb5096e38078555e3d6e6001a099c48c5203
Luna Luisa <sanchez.reyes.luna@gmail.com>
2019-02-21 11:21:01 -0500

    some figures

---
+++ Datelife_paper1_global.R
@@ -0,0 +1,9 @@
+fig_workflow_main <- "Fig. 1"
+fig_workflow_all_functions <- "Vignette Fig. 1"
+# make_datelife_query_plus_get_datelife_result
+fig_workflow_main_foot <- paste("Stylized DateLife workflow. Detailed steps are shown in",  fig_workflow_all_functions)
+
+fig_runtime_main <- "Fig. 2"
+fig_runtime_main_foot <- "Computation time of input processing and search across `DateLife`s chronogram database"
+fig_runtime_datagen <- "Fig. 3"
+fig_runtime_datasim <- "Fig. 4"
